fc18a18edc217a4dd39d720fde9c6fbba8fbcb7a
Bryce Anderson <bryce@crossroads-u-c.org>
2010-05-07 19:34:27 -0600

    Bare Rails 3 project tree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '5ffb25dfe8f9f906553ee5aa248be9bcd012a947670a28369a41fb86f3d1cb9e6e01b7126c5bcb16f7c267ad9a939fbd16a85ab3e1b82d98ab196076689ff523'
